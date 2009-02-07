4622098dd1c8ddf03139938dce8f847a9a71d61a
Aaron Bockover <abockover@novell.com>
2009-02-07 08:51:01 -0500

    Started building the real player UI

--- toolkit/moonshine-player.js
+++ toolkit/moonshine-player.js
@@ -1,48 +1,106 @@
 function __MoonshineBindInstance (control) {
     MtkContext.SilverlightOnLoad (control);
-
-    new MtkWindow (function () {
-        this.Add (new MtkVBox (function () {
-            this.PackStart (new MtkMediaElement, true);
-            this.PackStart (new MtkControlBar (function () {
-                this.PackStart (new MtkPlayPauseButton ({
-                    Events: { "click": function () alert ("OMG!") }
-                }));
-                this.PackStart (new MtkLabel ({ Text: "Label on the toolbar" }), true);
-                this.PackStart (new MtkButton ({
-                    With: function () this.Add (new MtkXaml ('<Canvas Width="50" Height="16" Background="purple"/>')),
-                    Events: { "click": function (o) o.Parent.Remove (o) }
-                }));
-                this.PackStart (new MtkButton (new MtkLabel ({ Text: "Click Me!" })));
-                this.PackStart (new MtkButton (function () this.Add (new MtkLabel ({ Text: "Another Button" }))));
-            }));
-        }));
-    });
+    new MoonshinePlayer;
 }
 
-function MtkControlBar (settings) {
-    this.XamlInitSource = '\
-        <Canvas> \
-            <Canvas.Background> \
-                <LinearGradientBrush StartPoint="0,0" EndPoint="0,1"> \
-                    <GradientStop Offset="0.0" Color="#f7f7f7"/> \
-                    <GradientStop Offset="0.5" Color="#e6e6e6"/> \
-                    <GradientStop Offset="0.5" Color="#ddd"/> \
-                    <GradientStop Offset="1.0" Color="#999"/> \
-                </LinearGradientBrush> \
-            </Canvas.Background> \
-        </Canvas> \
-    ';
+function MoonshinePlayer () {
+    MtkWindow.call (this);
 
-    MtkHBox.call (this, settings);
+    this.Container = new MtkVBox;
+    this.MediaElement = new MtkMediaElement;
+    this.Controls = new MoonshineControlBar;
 
-    this.Spacing = 3;
-    this.Padding = 3;
+    this.Container.PackStart (this.MediaElement, true);
+    this.Container.PackStart (this.Controls);
+    this.Add (this.Container);
+
+    this.Background = "black";
+
+    this.Controls.FullScreenButton.AddEventListener ("activated",
+        delegate (this, this.ToggleFullScreen));
 
     this.AfterConstructed ();
 }
 
-function MtkPlayPauseButton (settings) {
+function MoonshineControlBar () {
+    MtkToolBar.call (this);
+    var self = this;
+
+    this.Spacing = 8;
+    this.Padding = 2;
+
+    this.PlayPauseButton = new MoonshinePlayPauseButton;
+    this.SeekBar = new MoonshineSeekBar;
+    this.VolumeBar = new MoonshineVolumeBar;
+    this.FullScreenButton = new MtkButton (new MtkLabel ("Full Screen"));
+
+    this.PackStart (this.PlayPauseButton);
+    this.PackStart (new MtkHBox ({
+        Spacing: 16,
+        With: function () {
+            this.PackStart (self.SeekBar, true);
+            this.PackStart (self.VolumeBar);
+        }
+    }), true);
+    this.PackStart (this.FullScreenButton);
+
+    this.AfterConstructed ();
+}
+
+function MoonshineSeekBar () {
+    MtkSlider.call (this);
+}
+
+function MoonshineVolumeBar () {
+    MtkHBox.call (this);
+
+    this.Spacing = 4;
+
+    var speaker_path = '\
+        M 6,0 L 3,2.84 L 0,3.5 L 0,7.5 L 3,8.15 L 6,11 L 7,11 L 7,7.93 L  \
+        7,3.03 L 7,0 L 6,0 Z \
+    ';
+
+    var sound_wave_path = '\
+        M 7,3.03 C 7.75,3.26 8.34,4.29 8.34,5.5 C  \
+        8.34,6.7 7.75,7.7 7,7.93 C 7.1,7.97 7.22,8 7.34,8 C 8.26,8 8.99,6.88  \
+        9,5.5 C 9,4.12 8.26,3 7.34,3 C 7.22,3 7.1,2.99 7,3.03 Z M 8.5,1 C  \
+        8.32,1 8.16,1.03 8,1.09 C 9.13,1.51 10,3.32 10,5.5 C 10,7.67  \
+        9.13,9.48 8,9.9 C 8.16,9.96 8.32,10 8.5,10 C 9.88,10 10.99,7.98  \
+        11,5.5 C 11,3.01 9.87,1 8.5,1 Z \
+    ';
+
+    this.Icon = new MtkXaml ('\
+        <Canvas Width="13" Height="12"> \
+            <Path Canvas.Top=".5" Canvas.Left=".5" Stroke="#afff" \
+                StrokeThickness="1" Data="' + speaker_path + '"/> \
+            <Path Data="' + speaker_path + '"> \
+                <Path.Fill> \
+                    <LinearGradientBrush StartPoint="0,0" EndPoint="0,1"> \
+                        <GradientStop Offset="0.0" Color="#999"/> \
+                        <GradientStop Offset="0.45" Color="#777"/> \
+                        <GradientStop Offset="0.45" Color="#666"/> \
+                        <GradientStop Offset="1.0" Color="#333"/> \
+                    </LinearGradientBrush> \
+                </Path.Fill> \
+            </Path> \
+            <Path Canvas.Left="1" Data="' + sound_wave_path + '" Fill="#666"/> \
+        </Canvas> \
+    ');
+
+    this.Slider = new MtkSlider;
+    this.Slider.MinWidth = 60;
+    this.Slider.TroughHeight = 2;
+    this.Slider.TroughRadius = 1;
+    this.Slider.SliderWidth = 10;
+    this.Slider.SliderHeight = 6;
+    this.Slider.SliderRadius = 2;
+
+    this.PackStart (this.Icon);
+    this.PackStart (this.Slider);
+}
+
+function MoonshinePlayPauseButton (settings) {
     MtkButton.call (this, settings);
 
     this.Icon = new MtkXaml ('\
@@ -84,6 +142,11 @@ function MtkPlayPauseButton (settings) {
     ', this.XamlHost);
 
     this.Add (this.Icon);
+
+    this.Override ("OnActivated", function () {
+        this.$OnActivated$ ();
+    });
+
     this.AfterConstructed ();
 }
 
