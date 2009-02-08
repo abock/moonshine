f28b5db23ba3f3e552e2a75b0498bd8417fb02e1
Aaron Bockover <abockover@novell.com>
2009-02-07 21:50:56 -0500

    Implemented color styles, support mapping browser defined CSS 'system' colors to provide a somewhat native look and feel; implemented a helper CreateGradient function

--- toolkit/mtk/mtk-style.js
+++ toolkit/mtk/mtk-style.js
@@ -1,5 +1,70 @@
 var MtkStyle = {
 
+    DefaultColors: {
+        /*bg:    { normal: MtkColor.FromInt (0xd8d8d8) },
+        fg:    { normal: MtkColor.FromInt (0x444444) },
+        focus: { normal: MtkColor.FromInt (0x4d9f0e) }*/
+    },
+
+    system_colors: null,
+
+    get Colors () { return MtkStyle.system_colors || MtkStyle.DefaultColors; }, 
+
+    ShadeColorsFromNormal: function (colors) {
+        for (var set_name in colors) {
+            var set = colors[set_name];
+            set.light = MtkColor.Lighter (set.normal);
+            set.dark = MtkColor.Darker (set.normal);
+        }
+    },
+
+    Reload: function () {
+        MtkStyle.ShadeColorsFromNormal (MtkStyle.DefaultColors);
+        
+        MtkStyle.system_colors = null;
+
+        var css_color_map = {
+            "Window"        : "window_bg",
+            "WindowText"    : "window_fg",
+            "ButtonFace"    : "button_bg",
+            "ButtonText"    : "button_fg",
+            "Highlight"     : "highlight_bg",
+            "HighlightText" : "hightlight_fg",
+            "Scrollbar"     : "trough_bg"
+        };
+
+        var colors = {};
+        var elem = document.createElement ("div");
+      
+        for (var css in css_color_map) {
+            elem.style.color = css;
+            var style = document.defaultView.getComputedStyle (elem, null);
+            var color = style.getPropertyValue ("color");
+            colors[css_color_map[css]] = { normal: MtkColor.FromString (color) };
+        }
+
+        MtkStyle.ShadeColorsFromNormal (colors);
+        MtkStyle.system_colors = colors;
+    },
+
+    CreateGradient: function (widget, style) {
+        var offsets = [ 0, 0.4, 1 ];
+        var colors =  [ "light", "normal", "dark"];
+
+        var brush = widget.CreateXaml ("<LinearGradientBrush/>");
+        brush.StartPoint = "0,0";
+        brush.EndPoint = "0,1";
+
+        for (var i = 0, n = Math.min (offsets.length, colors.length); i < n; i++) {
+            var stop = widget.CreateXaml ("<GradientStop/>");
+            stop.Color = MtkColor.ToString (MtkStyle.Colors[style][colors[i]]);
+            stop.Offset = offsets[i];
+            brush.GradientStops.Add (stop);
+        }
+        
+        return brush;
+    },
+
     //
     // Font/Text
     //
