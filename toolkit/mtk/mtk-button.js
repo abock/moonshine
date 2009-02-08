55ba6e59a8d945793c5db9887490721b22085d90
Aaron Bockover <abockover@novell.com>
2009-02-07 21:51:24 -0500

    Use the new MtkStyle API to color the widgets

--- toolkit/mtk/mtk-button.js
+++ toolkit/mtk/mtk-button.js
@@ -18,10 +18,6 @@ function MtkButton (settings) {
     this.FocusOpacity = 1.0;
     this.InnerPadding = 5;
 
-    this.FillOffsets = [ 0.0, 0.5, 0.5, 1.0 ];
-    this.FillColors = [ "#e8e8e8", "#e0e0e0", "#d8d8d8", "#cdcdcd" ];
-    this.LightFillColors = [ "#e1e1e1", "#ececec", "#f4f4f4", "#f0f0f0" ];
-
     //
     // State
     //
@@ -32,14 +28,13 @@ function MtkButton (settings) {
     // Button UI and Interaction
     //
 
+    this.normal_fill_brush = null;
+    this.pressed_fill_brush = null;
+
     this.InitFromXaml ('\
         <Canvas Name="' + this.Name + '"> \
             <Canvas Name="' + this.Name + 'Style"> \
-                <Rectangle RadiusX="3" RadiusY="3" Stroke="#888"> \
-                    <Rectangle.Fill> \
-                        <LinearGradientBrush StartPoint="0,0" EndPoint="0,1" Name="' + this.Name + 'Fill"/> \
-                    </Rectangle.Fill> \
-                </Rectangle> \
+                <Rectangle RadiusX="3" RadiusY="3" Stroke="#888" Name="' + this.Name + 'Fill"/> \
                 <Rectangle RadiusX="2" RadiusY="2" Canvas.Left="1" Canvas.Top="1" Stroke="#7fff"/> \
             </Canvas> \
             <Canvas.Resources> \
@@ -63,6 +58,8 @@ function MtkButton (settings) {
             if (storyboard) {
                 storyboard.Begin ();
             }
+
+            this.StyleButton ();
         }));
         
         this.Xaml.AddEventListener ("mouseleave", delegate (this, function (o, args) {
@@ -72,44 +69,42 @@ function MtkButton (settings) {
                 storyboard.Begin ();
             }
 
-            if (this.IsPressed) {
-                this.FillButton (o, this.FillColors);
-            }
             this.IsPressed = false;
+            this.StyleButton ();
         }));
 
         this.Xaml.AddEventListener ("mouseleftbuttondown", delegate (this, function (o, args) {
             this.IsPressed = true;
-            this.FillButton (o, this.LightFillColors);
+            this.StyleButton ();
         }));
 
         this.Xaml.AddEventListener ("mouseleftbuttonup", delegate (this, function (o, args) {
             this.IsPressed = false;
-            this.FillButton (o, this.FillColors);
+            this.StyleButton ();
             this.OnActivated ();
         }));
 
-        var fill = this.XamlFind ("Fill").GradientStops;
-        for (var i = 0, n = Math.min (this.FillColors.length, this.FillOffsets.length); i < n; i++) {
-            var stop = this.CreateXaml ("<GradientStop/>");
-            stop.Color = this.FillColors[i];
-            stop.Offset = this.FillOffsets[i];
-            fill.Add (stop);
-        }
-
         this.XamlFind ("Style").Opacity = this.RestOpacity;
         this.Xaml.Cursor = "Hand";
+        this.StyleButton ();
 
         if (init_child) {
             this.Add (init_child);
         }
     });
 
-    this.FillButton = function (o, colors) {
-        var stops = this.XamlFind ("Fill").GradientStops;
-        for (var i = 0, n = Math.min (colors.length, stops.Count); i < n; i++) {
-            stops.GetItem (i).Color = colors[i];
+    this.StyleButton = function () {
+        if (!this.normal_fill_brush) {
+            this.normal_fill_brush = MtkStyle.CreateGradient (this, "button_bg");
         }
+
+        if (!this.pressed_fill_brush) {
+            this.pressed_fill_brush = MtkStyle.CreateGradient (this, "highlight_bg");
+        }
+
+        this.XamlFind ("Fill").Fill = this.IsPressed 
+            ? this.pressed_fill_brush 
+            : this.normal_fill_brush;
     };
 
     //
--- toolkit/mtk/mtk-toolbar.js
+++ toolkit/mtk/mtk-toolbar.js
@@ -1,21 +1,18 @@
 function MtkToolBar (settings) {
-    this.XamlInitSource = '\
-        <Canvas> \
-            <Canvas.Background> \
-                <LinearGradientBrush StartPoint="0,0" EndPoint="0,1"> \
-                    <GradientStop Offset="0.0" Color="#f7f7f7"/> \
-                    <GradientStop Offset="0.5" Color="#e6e6e6"/> \
-                    <GradientStop Offset="0.5" Color="#ddd"/> \
-                    <GradientStop Offset="1.0" Color="#999"/> \
-                </LinearGradientBrush> \
-            </Canvas.Background> \
-        </Canvas> \
-    ';
-
+    this.XamlInitSource = '<Canvas/>';
     MtkHBox.call (this, settings);
 
     this.Spacing = 3;
     this.Padding = 3;
+
+    this.Override ("OnRealize", function () {
+        this.$OnRealize$ ();
+        if (!this.IsRealized) {
+            return;
+        }
+
+        this.Xaml.Background = MtkStyle.CreateGradient (this, "window_bg");
+    });
 
     this.AfterConstructed ();
 }
