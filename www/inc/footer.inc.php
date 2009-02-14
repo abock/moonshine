23a72832641d2ce2e8ac7f11b8dbb92a6208417f
Jakub Steiner <jimmac@gmail.com>
2009-02-14 21:00:47 +0100

    improve the footer

--- www/inc/footer.inc.php
+++ www/inc/footer.inc.php
@@ -3,7 +3,7 @@
       <a id="mono-logo" href="http://www.go-mono.org">Mono</a>
       <a id="moonlight-logo" href="http://www.go-mono.org/moonlight">Moonlight</a>
     </div>
-    <div class="copy">&copy; 2009 <a href="http://abock.org">Aaron Bockover</a>. Site design <a href="http://jimmac.musichall.cz">Jakub Steiner</a>.</div>
+    <div class="copy"><div class="poweredby">.NET powered by Mono.</div><div>&copy; 2009 <a href="http://abock.org">Aaron Bockover</a>. Site design <a href="http://jimmac.musichall.cz">Jakub Steiner</a>.</div></div>
   </div><!-- footer -->
 
   </body>
