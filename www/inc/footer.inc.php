3c7de7dac30ce9553d6169312714632f7271165b
Jakub Steiner <jimmac@gmail.com>
2009-02-14 17:01:56 +0100

    footer logos & copy

--- www/inc/footer.inc.php
+++ www/inc/footer.inc.php
@@ -1,2 +1,10 @@
+  <div class="footer">
+    <div class="logos">
+      <a id="mono-logo" href="http://www.go-mono.org">Mono</a>
+      <a id="moonlight-logo" href="http://www.go-mono.org/moonlight">Moonlight</a>
+    </div>
+    <div class="copy">&copy; 2009 <a href="http://abock.org">Aaron Bockover</a>. Site design <a href="http://jimmac.musichall.cz">Jakub Steiner</a>.</div>
+  </div><!-- footer -->
+
   </body>
 </html>
