367291ed3a6434f6d8ec0fb166a15292c452b467
Jakub Steiner <jimmac@gmail.com>
2009-02-14 15:22:06 +0100

    fix the 'breathing' of the install buttons

--- www/inc/moonshine.js
+++ www/inc/moonshine.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   //prepare the install button
   $("#moonlight-banner, #install-host").remove();
   $("#install-buttons").append("<div id='button32'></div><div id='button64'></div>");
-  $("#button32, #button64").addClass('install-button').alphaHover(1000,'.8');
+  $("#button32, #button64").addClass('install-button').css('opacity','.8').alphaHover(1000,'.8');
 
 
   $('a').colorHover(500,'#f57900','#ffffff');
