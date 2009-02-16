d7e2d7840be1b041047cd32860ab7823bf1b685f
Jakub Steiner <jimmac@gmail.com>
2009-02-16 01:10:26 +0100

    nicer comments

--- www/inc/moonshine.js
+++ www/inc/moonshine.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   //prepare the install button
   $("#moonlight-banner, #install-host").remove();
   $("#install-buttons").append("<h2>Install Moonshine:</h2><div id='install-button'></div>");
-  //add an emblem if suse  
+  //add an emblem of the platform 
   var user_agent = navigator.userAgent.toLowerCase ();
   console.log(user_agent);
   var platforms = {'suse':'suse', 
@@ -15,15 +15,16 @@ $(document).ready(function () {
                    'mandriva':'mandriva', 
                    'fedora':'fedora'};
   for (x in platforms) {
-    // console.log(platforms[key]);
-
       if (user_agent.match(x)) {
         $('#install-button').append("<div class='" + platforms[x] + " distro'></div>")
       }
   }
   
+  //link hovers animated
   $('a').colorHover(500,'#f57900','#ffffff');
+  //screenshots
   $(".screenshots a").fancybox();
+  //slide in navigation
   $(".navigation").css("top","-40px").animate({top: '12px'},2000);
 });
 
