e7a28a9dc23835375b84b1979806adb52c56d44d
Aaron Bockover <abockover@novell.com>
2009-02-17 23:55:56 -0500

    Lots of polish and logic fixes for implementing the download page

--- www/inc/moonshine.js
+++ www/inc/moonshine.js
@@ -1,61 +1,91 @@
 $(document).ready (function () {
-    //prepare the install button
-    $("#moonlight-banner, #install-host").remove ();
-    $("#install-buttons").append ("<div id='install-button'></div>");
-        //add an emblem of the platform 
-        var user_agent = navigator.userAgent.toLowerCase ();
-        var platforms = {
-            'suse':      'suse', 
-            'sled':      'suse',
-            'mac':       'mac', 
-            'ubuntu':    'ubuntu', 
-            'mandriva':  'mandriva', 
-            'fedora':    'fedora',
-            'foresight': 'foresight'
-        };
-        
-        for (x in platforms) {
-            if (!user_agent.match (x)) {
-                continue;
+
+    var user_agent = navigator.userAgent.toLowerCase ();
+    var platforms = {
+        "suse":      "openSUSE",
+        "sled":      "openSUSE",
+        "opensuse":  "openSUSE",
+        "osx":       "Mac OS X",
+        "ubuntu":    "Ubuntu",
+        "mandriva":  "Mandriva",
+        "fedora":    "Fedora",
+        "foresight": "Foresight",
+        "firefox":   "Firefox",
+        "generic":   "Source Code"
+    };
+    
+    var platform_id = "generic";
+    var platform_name = platforms[platform_id];
+    
+    for (i in platforms) {
+        if (user_agent.match (i)) {
+            platform_id = i;
+            platform_name = platforms[i];
+            if (platform_id == "suse" || platform_id == "sled") {
+                platform_id = "opensuse";
             }
-            
-            $('#install-button').append ("<div class='" + platforms[x] + " distro'></div>").click (function () {
-                // launch the install
-            }).mousedown (function () {
-                //move the emblem along ;)
-                $('#install-button .distro').css ('top','25px');
-            }).mouseup (function () {
-                var top = $('#install-button .distro').css ('top');
-                $('#install-button .distro').css ('top','23px');
-            });
-            
             break;
         }
-  
+    }
+    
+    $("#moonlight-banner, #install-host").remove ();
+    $("#install-buttons").append ("<div id=\"install-button\"></div>");
+    $(".install").each (function (i, o) {
+        var name = $(o).attr('class').split(' ')[1];
+        $(".distro-list").append ("<div class=\"distro-button distro-button-"
+            + name + "\"><img src=\"images/distro-logos/48x48/" 
+            + name + ".png\" /><p>" + platforms[name] + "</p></div>");
+        var button = $(".distro-button-" + name).click (function () {
+            if ($(this).hasClass ("chosen")) {
+                return;
+            }
+            
+            $(".distro-button").removeClass ("chosen");
+            $(".install").hide (300);
+            $("." + name).show (300);
+            $(this).addClass ("chosen");
+        });
+
+        if (name != platform_id) {
+            $(o).hide ();
+        } else {
+            button.addClass ("chosen");
+        }
+    });
+    
+    $("#install-button").append ("<div class=\"button-" + platform_id + " distro\"></div>").click (function () {
+        window.location = "/download.php";
+    }).mousedown (function () {
+        $("#install-button .distro").css ("top", "25px");
+    }).mouseup (function () {
+        var top = $("#install-button .distro").css ("top");
+        $("#install-button .distro").css ("top", "23px");
+    });
+    
     // link hovers animated
-    $('a').colorHover (500, '#f57900', '#ffffff');
+    $("a").colorHover (500, "#f57900", "#ffffff");
     
     //screenshots
     $(".screenshots a").fancybox ();
     
     //slide in navigation
-    $(".navigation-animation").css ("top","-40px").animate ({top: '12px'}, 2000);
+    $(".navigation-animation").css ("top", "-40px").animate ({top: "12px"}, 2000);
 });
 
 (function ($) {
     $.fn.colorHover = function (animtime, fromColor, toColor) {
         $(this).hover (function () {
-            return $(this).css ('color', fromColor).stop ().animate ({'color': toColor}, animtime);
+            return $(this).css ("color", fromColor).stop ().animate ({"color": toColor}, animtime);
         }, function () {
-            return $(this).stop ().animate ({'color': fromColor}, animtime);
+            return $(this).stop ().animate ({"color": fromColor}, animtime);
         });
     };
   
     $.fn.alphaHover = function (animtime, fromAlpha) {
         $(this).hover (function () {
-            return $(this).css ('opacity', fromAlpha).stop ().animate ({'opacity': '1'}, animtime);
+            return $(this).css ("opacity", fromAlpha).stop ().animate ({"opacity": "1"}, animtime);
         }, function () {
-            return $(this).stop ().animate ({'opacity': fromAlpha}, animtime);
+            return $(this).stop ().animate ({"opacity": fromAlpha}, animtime);
         });
     };
 }) (jQuery);
